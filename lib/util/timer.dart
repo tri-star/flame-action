cd13db15136836a1b934e99de5cbbf088c768cef
tristar <tristar@urban-theory.net>
2020-07-01 01:53:43 +0900

    TimeoutTimerにタイムアウト時のCallbackを実行する機能を追加

--- lib/util/timer.dart
+++ lib/util/timer.dart
@@ -1,16 +1,41 @@
+import 'package:flutter/cupertino.dart';
+
 class TimeoutTimer {
   double timeout;
   double _current;
+  VoidCallback _callback;
+  bool _finished;
 
-  TimeoutTimer(this.timeout) : _current = 0;
+  TimeoutTimer(this.timeout, {VoidCallback callback})
+      : _current = 0,
+        _finished = false,
+        _callback = callback;
+
+  void init(VoidCallback callback) {
+    _current = 0;
+    _finished = false;
+    _callback = callback;
+  }
 
   void update() {
+    if (_finished) {
+      return;
+    }
+
     //TODO: 1フレームの正式な値で更新する(定数など)
     _current += 0.016;
+
+    if (isDone() && !_finished) {
+      _finished = true;
+      if (_callback != null) {
+        _callback();
+      }
+    }
   }
 
   void reset() {
     this._current = 0;
+    this._finished = false;
   }
 
   bool isDone() {
