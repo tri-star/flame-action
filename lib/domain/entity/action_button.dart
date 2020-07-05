becd8094b00becea71416f182849040fb2229679
tristar <tristar@urban-theory.net>
2020-07-05 09:19:25 +0900

    未使用のimportの削除

--- lib/domain/entity/action_button.dart
+++ lib/domain/entity/action_button.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flame_action/engine/image/sprite.dart';
 import 'package:flame_action/engine/image/sprite_resolver.dart';
 import 'package:flame_action/engine/input_event.dart';
--- lib/engine/world.dart
+++ lib/engine/world.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'dart:ui';
 
 import 'package:flame_action/engine/entity/entity.dart';
