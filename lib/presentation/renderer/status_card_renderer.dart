294cd6afb5b1b917033b643ee8fcbca2aff07015
tristar <tristar@urban-theory.net>
2020-07-07 10:56:59 +0900

    StatusCardに顔を表示する対応

--- lib/presentation/renderer/status_card_renderer.dart
+++ lib/presentation/renderer/status_card_renderer.dart
@@ -3,10 +3,22 @@ import 'dart:ui';
 import '../../domain/entity/ui/status_card.dart';
 import '../../engine/camera.dart';
 import '../../engine/entity/figting_unit.dart';
+import '../../engine/image/sprite.dart';
+import '../../engine/presentation/flame/flame_sprite_sheet.dart';
 import '../../engine/presentation/renderer.dart';
 
 class StatusCardRenderer extends Renderer<StatusCard> {
   final double CARD_OPACITYT = 0.8;
+
+  final Map<String, int> _faceIndexMapping = {'player': 0, 'enemy01': 1};
+
+  static FlameSpriteSheet _faceSpriteSheet;
+
+  StatusCardRenderer() {
+    if (_faceSpriteSheet == null) {
+      _faceSpriteSheet = FlameSpriteSheet('faces.png', 20, 20, 5, 1);
+    }
+  }
 
   @override
   void render(Canvas canvas, Camera camera, StatusCard subject) {
@@ -23,6 +35,19 @@ class StatusCardRenderer extends Renderer<StatusCard> {
     drawY += 5;
     canvas.drawRect(Rect.fromLTWH(drawX, drawY, 25, 25),
         Paint()..color = Color.fromRGBO(200, 200, 200, cardOpacity));
+
+    int faceIndex = _faceIndexMapping[subject.getTarget().getEntityName()];
+    Sprite sprite = _faceSpriteSheet.getSpriteByIndex(faceIndex);
+    if (sprite != null) {
+      // TODO: 座標の指定の仕方を工夫する
+      sprite.x = drawX + 2;
+      sprite.y = drawY + 2;
+      // TODO: Sprite内で半透明の制御を隠蔽する
+      sprite.paint = Paint()
+        ..colorFilter = ColorFilter.mode(
+            Color.fromRGBO(255, 255, 255, cardOpacity), BlendMode.modulate);
+      sprite.render(canvas, null);
+    }
 
     drawX += 30;
 
