8fac22232e79b2bab77411953ef11de0fbb04f43
tristar <tristar@urban-theory.net>
2020-07-13 09:23:15 +0900

    プレイヤーに対してもダメージを表示するように対応

--- lib/entity/generic_bullet.dart
+++ lib/entity/generic_bullet.dart
@@ -33,10 +33,17 @@ class GenericBullet extends Entity {
   @override
   void onCollide(WorldContext context, CollisionEvent event) {
     if (event.source is Player) {
-      Vector3d force = Vector3d(dimension == Dimension.LEFT ? 2 : -2, -10, 0);
+      Vector3d force = Vector3d(
+          event.source.getDimension() == Dimension.LEFT ? 2 : -2, -10, 0);
       DamageCommand(event.source, 1234, force: force).execute();
       context.addEntity(
           context.entityFactory.create('particle_damage01', x, y, z));
+
+      //TODO: ダメージコマンドに含める
+      context.addEntity(context.entityFactory.create(
+          'pop_with_gravity_string', x, y, z + 5,
+          options: {'message': '1234'}));
+
       dispose();
     }
   }
